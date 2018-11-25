94776c06c94a2940337f88109426d49a87bb1d74
Isaac Liscia <isaacliscia@MacBook-Air-de-Isaac.local>
2018-11-25 18:16:24 +0200

    add home.html & update views

--- hive/hive/urls.py
+++ hive/hive/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from app1 import views
+from first_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hive/', include('hive.urls')),
+    path('first_app/', include('first_app.urls')),
 ]
