22e454d6741d3ee7fff3b2ead91c45fc4d2286d3
orone lussato <oronelussato@orones-MacBook-Pro.local>
2018-11-25 18:17:42 +0200

    add models

--- hive/first_app/models.py
+++ hive/first_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+	first_name = models.CharField(max_length=264)
+	last_name = models.CharField(max_length=264)
+	bio = models.CharField(max_length=264)
+
+	def __str__(self):
+		return self.first_name
+
+
+class Feed(models.Model):
+	text = models.TextField(max_length=140)
+	date = models.DateField(auto_now=True)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	like = models.CharField(max_length=26)
