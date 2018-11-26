6191da3dbb3a74237081e7b164f82c16c5785518
Isaac Liscia <isaacliscia@MacBook-Air-de-Isaac.local>
2018-11-26 18:29:57 +0200

    implemente home.html

--- hive/first_app/views.py
+++ hive/first_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from first_app.models import User,UserProfileInfo,Feed
 def index(request):
 	return render(request, 'home.html')
+
+def homepage(request):
+	feeds = {'feed' : Feed.objects.all().order_by('text')[:20]
+	}
+	return render(request, 'home.html', feeds)
